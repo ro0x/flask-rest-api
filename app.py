b2f972ddff83fd1d68467773da9c0da8e901c2ef
Rick O <rick@ro0x.com>
2017-08-20 20:34:48 -0700

    Changing to PostgreSQL and adding psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +11,8 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.secret_key = '29#H8fbQcf/~kWK~NXbg[T=_q.3m:B.>RKy[-nH]\S}YcfGS[9~>D$:4r*YdZM'
 api = Api(app)
 
